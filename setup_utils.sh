07d5f400f39e6518fc4a8d437581e9d88873a758
Marco Valente <marco.valente1992@gmail.com>
2016-12-22 18:44:36 +0100

    Setting up initialisation scripts

--- setup_utils.sh
+++ setup_utils.sh
@@ -2,9 +2,11 @@ CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CMDMGRDIR=$CURRENTDIR/cmd-mgr
 COMMONDIR=$CURRENTDIR/common
+SCRIPTSDIR=$CURRENTDIR/scripts
+
+cat $SCRIPTSDIR/init/hellomarco.txt
 
 export PYTHONPATH="${PYTHONPATH}:$COMMONDIR"
-
 
 #alias definitions
 alias cmd-mgr='python $CMDMGRDIR/cmd-mgr.py'
