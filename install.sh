b3c054d698013420200fc736d25e7c2ff386d1be
Marco Valente <marco.valente1992@gmail.com>
2019-01-12 11:06:20 +0100

    Updating installation files and procedures

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-echo Installing python argcomplete
+echo "Updating apt-get packages"
+sudo apt-get update
+echo ""
+echo "Install new apt-get packages"
+cat packages.txt | xargs sudo apt-get install
+echo ""
+echo "Installing python argcomplete"
 sudo pip install argcomplete
 sudo activate-global-python-argcomplete
